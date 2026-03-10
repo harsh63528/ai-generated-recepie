78a0f1315cd5722ee4b1377995b727d50ba9b198
harsh singh <vduharsh123@gmail.com>
2026-03-10 19:19:42 +0530

    started creating bacend structure

---
+++ server/server.js
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express';
+import cors from 'cors';
+import App from './src/app.js';
+
+const app = express();
+const PORT = process.env.PORT || 5000;  
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
